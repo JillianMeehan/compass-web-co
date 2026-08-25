08baba7dcbee2ef68473d1d350d82f5609d45c58
jillianmeehan <jillianmeehan.dev@gmail.com>
2026-08-24 21:22:17 -0400

    Transferred  template and applied branding guidelines.
    Applied color palette, logos, and animation to the navbar logo.

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
-console.log("Is this thing on?");
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    }
+  });
+});
+
+const hiddenElements = document.querySelectorAll(
+  ".service-card, .pricing-card, .maintenance-card, .portfolio-card, .testimonial-card, .industry-card",
+);
+
+hiddenElements.forEach((el) => observer.observe(el));
